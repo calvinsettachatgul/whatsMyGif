979d3ac325cbcef81ac0431ddf9a06d1d34cdd4e
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-16 13:25:49 -0700

    going to completing the template

--- client/templates/RWDevConTemplate.xml.js
+++ client/templates/RWDevConTemplate.xml.js
@@ -1,9 +1,21 @@
+
 var Template = function() { return `<?xml version="1.0" encoding="UTF-8" ?>
   <document>
     <catalogTemplate>
       <banner>
         <title>RWDevConHighlights</title>
       </banner>
+      //add stuff here
+      //1.
+      <list>
+        <section>
+          //2.
+    <listItemLockup>
+      <title>Inspiration Videos</title>
+      <decorationLabel>13</decorationLabel>
+    </listItemLockup>
+        </section>
+      </list>
     </catalogTemplate>
   </document>`
 }
