c6929ba66462863e2f3c1c6a6150c6a994506465
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2016-04-16 12:49:20 -0700

    first commit

---
+++ client/js/application.js
@@ -0,0 +1,24 @@
+
+
+App.onLaunch = function(options) {
+  // 1
+  var alert = createAlert("Welcome to the whatsMyGif", ""); //leaving 2nd parameter with an empty string
+  navigationDocument.presentModal(alert);
+}
+
+// 2
+var createAlert = function(title, description) {
+  var alertString = `<?xml version="1.0" encoding="UTF-8" ?>
+    <document>
+      <alertTemplate>
+        <title>${title}</title>
+        <description>${description}</description>
+        <button>
+        <text>What are your answers?</text>
+        </button>
+      </alertTemplate>
+    </document>`
+    var parser = new DOMParser();
+    var alertDoc = parser.parseFromString(alertString, "application/xml");
+    return alertDoc
+}
---
+++ whatsMyGif/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  whatsMyGif
+//
+//  Created by Calvin Settachatgul on 4/16/16.
+//  Copyright © 2016 Calvin Settachatgul. All rights reserved.
+//
+
+import UIKit
+import TVMLKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, TVApplicationControllerDelegate {
+
+    var window: UIWindow?
+    
+    var appController: TVApplicationController?
+    static let TVBaseURL = "http://localhost:9001/"
+    static let TVBootURL = "\(AppDelegate.TVBaseURL)js/application.js"
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        
+        // 1
+        let appControllerContext = TVApplicationControllerContext()
+        
+        // 2
+        guard let javaScriptURL = NSURL(string: AppDelegate.TVBootURL) else {
+            fatalError("unable to create NSURL")
+        }
+        appControllerContext.javaScriptApplicationURL = javaScriptURL
+        appControllerContext.launchOptions["BASEURL"] = AppDelegate.TVBaseURL
+        
+        // 3
+        appController = TVApplicationController(context: appControllerContext, window: window, delegate: self)
+        return true
+    }
+
+
+}
+
